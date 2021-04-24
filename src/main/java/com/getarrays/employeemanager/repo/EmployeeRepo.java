bba1ff6399c7bb77655c98bc4f18dc9222b0fd35
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-04-24 18:14:08 +0200

    added crud methods to service class

--- src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -1,4 +1,17 @@
 package com.getarrays.employeemanager.repo;
 
-public interface EmployeeRepo {
+import java.util.List;
+import java.util.Optional;
+
+import com.getarrays.employeemanager.model.Employee;
+
+public interface EmployeeRepo{
+
+  void deleteEmployeeById(Long id);
+
+  Employee save(Employee employee);
+
+  List<Employee> findAll();
+
+  Optional<Employee> findEmployeeById(Long id);
 }
--- src/main/java/com/getarrays/employeemanager/service/EmployeeService.java
+++ src/main/java/com/getarrays/employeemanager/service/EmployeeService.java
@@ -1,5 +1,10 @@
 package com.getarrays.employeemanager.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import com.getarrays.employeemanager.exception.UserNotFoundException;
+import com.getarrays.employeemanager.model.Employee;
 import com.getarrays.employeemanager.repo.EmployeeRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +17,26 @@ public class EmployeeService {
   @Autowired
   public EmployeeService(EmployeeRepo employeeRepo){
     this.employeeRepo = employeeRepo;
+  }
+
+  public Employee addEmployee(Employee employee){
+    employee.setEmployeeCode(UUID.randomUUID().toString());
+    return employeeRepo.save(employee);
+  }
+
+  public List<Employee> findAllEmployees(){
+    return employeeRepo.findAll();
+  }
+
+  public Employee updateEmployee(Employee employee){
+    return employeeRepo.save(employee);
+  }
+
+  public Employee findEmployeeById(Long id){
+    return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User not found."));
+  }
+
+  public void deleteEmployee(Long id){
+    employeeRepo.deleteEmployeeById(id);
   }
 }
