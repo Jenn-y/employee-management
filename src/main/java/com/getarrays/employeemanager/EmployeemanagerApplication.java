97ec344616034596631e5489b99203d39cea2de3
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-05-02 00:46:27 +0100

    added cors configuration file

--- src/main/java/com/getarrays/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -3,7 +3,6 @@ package com.getarrays.employeemanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"java.com.getarrays.employeemanager"})
 public class EmployeemanagerApplication {
@@ -11,5 +10,4 @@ public class EmployeemanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
-
 }
